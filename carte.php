5802267e4a198f3e4c995abb45118298e74ffacd
dylnm <dyln.mnsri@gmail.com>
2025-12-11 02:18:13 +0100

    modif pour validation accessibilité, html

--- carte.php
+++ carte.php
@@ -3,7 +3,10 @@ $h1="Carte des activités";
 $title="Carte des activités";
 $description="Les activité du Val-d'Oise affiché sur une carte.";
 $css="carte";
-$cdn = '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">';
+$cdn = '<link rel="preconnect" href="https://unpkg.com" crossorigin>
+<link rel="preload" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" as="style"/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
+';
 include "includes/pageParts/header.php";
 ?>
 
@@ -22,7 +25,7 @@ include "includes/pageParts/header.php";
 let mapStyle = "<?=$style?>"; // récupère "light" ou "dark" depuis le header
 </script>
 
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script defer src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <script src="includes/script/map.js" defer></script>
     <script src="includes/script/pins.js" defer></script>
 </main>
